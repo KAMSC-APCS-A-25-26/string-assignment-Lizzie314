4b60c7cf9cd4a5741302a16fc8686e66264e67ab
Lizzie314 <elizabethbippley@gmail.com>
2025-10-07 18:26:57 -0400

    Update WordCensor.java

--- src/main/java/WordCensor.java
+++ src/main/java/WordCensor.java
@@ -6,5 +6,13 @@ public class WordCensor {
         // TODO: Replace each banned word with exactly ***: dang, nuts, oops, yikes
         // TODO: Print the result prefixed with: Censored:
         // NOTE: Tests require exactly three asterisks (no ****) for every replacement
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String original = sc.nextLine();
+        String dang = original.replace("dang", "***");
+        String nuts = dang.replace("nuts", "***");
+        String oops = nuts.replace("oops", "***");
+        String yikes = oops.replace("yikes", "***");
+        System.out.println("Censored: " + yikes);
     }
 }
